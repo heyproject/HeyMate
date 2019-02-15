4194c55b04b2a6b61dc0ba2c8aca37cef1a24a08
farabi <farabi.djauhari@gmail.com>
2019-02-16 09:47:50 +1100

    add phone input

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
-
+import { ImageBackground, StyleSheet, Platform, Image, Text, View, SafeAreaView, Keyboard,
+                                                                                   KeyboardAvoidingView,
+                                                                                   TouchableWithoutFeedback } from 'react-native';
+// native base imports
+import {
+  Container,
+  Item,
+  Input,
+} from 'native-base';
+import { Icon } from 'react-native-elements'
 import firebase from 'react-native-firebase';
-import { Formik } from 'formik';
+import { Formik, FormikProps, Form, Field } from 'formik';
 
 
 export default class App extends React.Component {
@@ -21,41 +29,52 @@ export default class App extends React.Component {
 
   render() {
     return (
-    <ImageBackground source={require('./assets/img/background.png')} style={styles.container} >
-      <ScrollView>
-        <View style={styles.container}>
-            <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-            <Text style={styles.welcome}>
-              Welcome to {'\n'} React Native Firebase
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit App.js
-            </Text>
-            {Platform.OS === 'ios' ? (
-              <Text style={styles.instructions}>
-                Press Cmd+R to reload,{'\n'}
-                Cmd+D or shake for dev menu
-              </Text>
-            ) : (
-              <Text style={styles.instructions}>
-                Double tap R on your keyboard to reload,{'\n'}
-                Cmd+M or shake for dev menu
-              </Text>
-            )}
-        </View>
-      </ScrollView>
+    <ImageBackground source={require('./assets/img/background.png')} style={styles.containerBackground} >
+        <SafeAreaView style={styles.container}>
+          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
+          <KeyboardAvoidingView
+               style={styles.container}
+               behavior='padding' enabled >
+              <TouchableWithoutFeedback
+                style={styles.container}
+                onPress={Keyboard.dismiss}>
+                <View style={styles.bottom}>
+                    {/* Phone input with native-base */}
+                    <Item rounded style={styles.itemStyle}>
+                      <Icon
+                          raised
+                          name='phone'
+                          type='font-awesome'
+                          color='#f50'
+                      />
+                      <Input placeholder='+61451554433'
+                               placeholderTextColor='#adb4bc'
+                               keyboardType={'phone-pad'}
+                               returnKeyType='done'
+                               autoCapitalize='none'
+                               autoCorrect={false}
+                               secureTextEntry={false} style={styles.inputStyle}/>
+                    </Item>
+                  </View>
+                </TouchableWithoutFeedback>
+          </KeyboardAvoidingView>
+        </SafeAreaView>
       </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  containerBackground: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    width: null,
-    height: null,
+    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
   logo: {
     height: 120,
@@ -64,27 +83,24 @@ const styles = StyleSheet.create({
     padding: 10,
     width: 135,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36,
+    width: 300,
+  },
+  iconStyle: {
+    color: '#fff',
+    fontSize: 28,
+    marginLeft: 15,
+  },
+  itemStyle: {
+    position: 'absolute',
+    bottom:0,
+  },
+  inputStyle: {
+    fontSize: 22,
+    fontWeight: 'bold',
     color: '#fff',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#fff',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
 });
